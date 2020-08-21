e548f88c2df65e1e77f525403fa5058e00398e8b
Bananberg <danandreaskallin@gmail.com>
2020-08-21 11:06:42 +0200

    just a save commit pls ignore.

---
+++ src/com/company/Drawable.java
@@ -0,0 +1,8 @@
+package com.company;
+
+// Intent is that this class (or Interface if that's what it should be...)
+// enables things to be drawn onto our Canvas
+
+public class Drawable {
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,7 @@
 package com.company;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
